91bfe05a8e95b0423eb195a6f1daaac75338d188
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-06-05 01:12:34 -0300

    Enpoints de busca e exclusão de produtos por id

--- src/main/java/com/fiap/trabalho1/fiap/controllers/ClientController.java
+++ src/main/java/com/fiap/trabalho1/fiap/controllers/ClientController.java
@@ -1,19 +1,13 @@
 package com.fiap.trabalho1.fiap.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fiap.trabalho1.fiap.entities.Client;
 import com.fiap.trabalho1.fiap.external.request.ClientRequest;
 import com.fiap.trabalho1.fiap.usecases.client.CreateClientUseCase;
 import com.fiap.trabalho1.fiap.usecases.client.ListAllClientsUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
@@ -26,14 +20,14 @@ public class ClientController {
 	private CreateClientUseCase createClientUseCase;
 	
 	@PostMapping
-	public ResponseEntity<Client> salvarEvento(@RequestBody ClientRequest request) {
+	public ResponseEntity<Client> saveClient(@RequestBody ClientRequest request) {
 		Client clientSaved = this.createClientUseCase.execute(request.getName(), request.getCPF(), request.getEmail());
 		return ResponseEntity.ok(clientSaved);
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<Page<Client>> listAllClients(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
-														    @RequestParam(value = "size", required = false, defaultValue = "20") int size) {
+													   @RequestParam(value = "size", required = false, defaultValue = "20") int size) {
 		Page<Client> clients = this.listAllClientsUseCase.listAllClients(page, size);
     	if (clients.isEmpty()) {
     		return ResponseEntity.notFound().build();
--- src/main/java/com/fiap/trabalho1/fiap/controllers/ProductController.java
+++ src/main/java/com/fiap/trabalho1/fiap/controllers/ProductController.java
@@ -1,18 +1,15 @@
 package com.fiap.trabalho1.fiap.controllers;
 
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fiap.trabalho1.fiap.entities.Product;
 import com.fiap.trabalho1.fiap.external.request.ProductRequest;
-import com.fiap.trabalho1.fiap.usecases.product.CreateProductUseCase;
-import com.fiap.trabalho1.fiap.usecases.product.ListAllProductsUseCase;
+import com.fiap.trabalho1.fiap.usecases.product.*;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -20,11 +17,20 @@ public class ProductController {
 	
     private final CreateProductUseCase createProductUseCase;
     private final ListAllProductsUseCase listAllProductsUseCase;
-    
+    private final FindProdcutByIdUseCase findProdcutByIdUseCase;
+    private final EditProductUseCase editProductUseCase;
+    private final DeleteProdcutByIdUseCase deleteProdcutByIdUseCase;
+
     public ProductController(CreateProductUseCase createProductUseCase,
-    						 ListAllProductsUseCase listAllProductsUseCase) {
+    						 ListAllProductsUseCase listAllProductsUseCase,
+                             FindProdcutByIdUseCase findProdcutByIdUseCase,
+                             EditProductUseCase editProductUseCase,
+                             DeleteProdcutByIdUseCase deleteProdcutByIdUseCase) {
         this.createProductUseCase = createProductUseCase;
         this.listAllProductsUseCase = listAllProductsUseCase;
+        this.findProdcutByIdUseCase = findProdcutByIdUseCase;
+        this.editProductUseCase = editProductUseCase;
+        this.deleteProdcutByIdUseCase = deleteProdcutByIdUseCase;
     }
 
     @PostMapping
@@ -37,7 +43,33 @@ public class ProductController {
         );
         return ResponseEntity.ok(product);
     }
-    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable String id) {
+        Optional<Product> productOptional = this.findProdcutByIdUseCase.execute(UUID.fromString(id));
+        if (productOptional.isPresent()) {
+            return ResponseEntity.ok(productOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> editProductById(@PathVariable String id, @RequestBody ProductRequest request) {
+        UUID productId = UUID.fromString(id);
+        if (this.findProdcutByIdUseCase.execute(productId).isPresent()) {
+            Product product = editProductUseCase.execute(
+                    productId,
+                    request.getName(),
+                    request.getValue(),
+                    request.getDescription(),
+                    request.getCategoryId()
+            );
+            return ResponseEntity.ok(product);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
     @GetMapping
 	public ResponseEntity<Page<Product>> listAllProdcuts(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 														 @RequestParam(value = "size", required = false, defaultValue = "20") int size) {
@@ -48,5 +80,11 @@ public class ProductController {
     	
     	return ResponseEntity.ok(products);
 	}
-    
+
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteCategoryById(@PathVariable String id) {
+        this.deleteProdcutByIdUseCase.execute(UUID.fromString(id));
+        return HttpStatus.OK;
+    }
+
 }
